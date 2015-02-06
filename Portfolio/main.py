2db1960512f28f46871d1fcd469a35a772eb2417
Vivian <vivian.brown@gmail.com>
2015-02-06 17:54:00 -0500

    More styling and light content edits. Move template files to their own folder.

--- Portfolio/main.py
+++ Portfolio/main.py
@@ -13,56 +13,56 @@ class MainPage(webapp.RequestHandler):
     """Render the main landing page where users can view instructions and create a new one."""
     def get(self):
         template_values = {}
-        path = os.path.join(os.path.dirname(__file__), 'index.html')
+        path = os.path.join(os.path.dirname(__file__) + '/templates/', 'index.html')
         self.response.out.write(template.render(path, template_values))
-        
+
 class MetaTicTacToePage(webapp.RequestHandler):
     """Render the main landing page where users can view instructions and create a new one."""
     def get(self):
         template_values = {}
-        path = os.path.join(os.path.dirname(__file__), 'metatictactoe.html')
+        path = os.path.join(os.path.dirname(__file__) + '/templates/', 'metatictactoe.html')
         self.response.out.write(template.render(path, template_values))
-        
+
 class BioacousticsPage(webapp.RequestHandler):
     """Render the main landing page where users can view instructions and create a new one."""
     def get(self):
         template_values = {}
-        path = os.path.join(os.path.dirname(__file__), 'bioacoustics.html')
+        path = os.path.join(os.path.dirname(__file__) + '/templates/', 'bioacoustics.html')
         self.response.out.write(template.render(path, template_values))
-        
+
 class MtPage(webapp.RequestHandler):
     """Render the main landing page where users can view instructions and create a new one."""
     def get(self):
         template_values = {}
-        path = os.path.join(os.path.dirname(__file__), 'mt.html')
+        path = os.path.join(os.path.dirname(__file__) + '/templates/', 'mt.html')
         self.response.out.write(template.render(path, template_values))
-        
+
 class FunnyWordsPage(webapp.RequestHandler):
     """Render the main landing page where users can view instructions and create a new one."""
     def get(self):
         template_values = {}
-        path = os.path.join(os.path.dirname(__file__), 'funnywords.html')
+        path = os.path.join(os.path.dirname(__file__) + '/templates/', 'funnywords.html')
         self.response.out.write(template.render(path, template_values))
-        
+
 class OpenDisclosurePage(webapp.RequestHandler):
     """Render the main landing page where users can view instructions and create a new one."""
     def get(self):
         template_values = {}
-        path = os.path.join(os.path.dirname(__file__), 'opendisclosure.html')
+        path = os.path.join(os.path.dirname(__file__) + '/templates/', 'opendisclosure.html')
         self.response.out.write(template.render(path, template_values))
 
 class WritingPage(webapp.RequestHandler):
     """Render the main landing page where users can view instructions and create a new one."""
     def get(self):
         template_values = {}
-        path = os.path.join(os.path.dirname(__file__), 'writing.html')
+        path = os.path.join(os.path.dirname(__file__) + '/templates/', 'writing.html')
         self.response.out.write(template.render(path, template_values))
-        
+
 class ProjectsPage(webapp.RequestHandler):
     """Render the main landing page where users can view instructions and create a new one."""
     def get(self):
         template_values = {}
-        path = os.path.join(os.path.dirname(__file__), 'projects.html')
+        path = os.path.join(os.path.dirname(__file__) + '/templates/', 'projects.html')
         self.response.out.write(template.render(path, template_values))
 
 
