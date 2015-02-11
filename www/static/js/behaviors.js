217ac17126c93603aa1e02d4e17883661f79d6dc
Vivian <vivian.brown@gmail.com>
2015-02-11 14:40:17 -0800

    Update project pages. Add interactive hover on homepage. Upload senior thesis.

--- www/static/js/behaviors.js
+++ www/static/js/behaviors.js
@@ -1,12 +1,24 @@
 $(function() {
   $('.homepage-thumbs .thumb').hover(function(thumb) {
-    var thumb = $(this);
-    var teaser = thumb.find('.teaser');
 
-    teaser.css({
-      top: (thumb.innerHeight() - teaser.innerHeight())/2
-    });
+    // If not mobile...
+    if (!window.matchMedia('all and (max-width: 740px)').matches) {
+      var thumb = $(this);
+      var teaser = thumb.find('.teaser');
 
-    teaser.toggle();
+      // Center the teaser text within the thumbnail.
+      teaser.css({
+        'padding-top': (thumb.innerHeight() - teaser.innerHeight())/2,
+        'padding-bottom': (thumb.innerHeight() - teaser.innerHeight())/2
+      });
+
+      // Show the teaser text and make it a button.
+      teaser.toggle();
+      teaser.toggleClass('js-hover');
+      teaser.click(function() {
+        window.location = $(this).find('a').attr('href');
+      })
+    }
+
   })
 });
